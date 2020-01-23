130f23e0e7b4d86c95fed18adbc803ea4fe9f7de
Numero03% <lucasmtvilain@gmail.com>
2020-01-23 14:38:33 +0100

    allumer les pixels

--- ConvertImage.py
+++ ConvertImage.py
@@ -1,15 +1,19 @@
 import cv2
 import numpy
 import rechercheMatrice
+import neopixel
+import board
+import time
 
 
 def convert_file(ImageUrl):
+
 
     # récupération de l'image sur le serveur
     img = cv2.imread(
         ImageUrl,
         cv2.IMREAD_UNCHANGED)
-    cv2.imshow("Resized image", img)
+    #cv2.imshow("Resized image", img)
 
     # Reduction de la taille de l'image
     scale_percent = 1.1  # percent of original size
@@ -20,10 +24,13 @@ def convert_file(ImageUrl):
         width = int(width / scale_percent)
         height = int(height / scale_percent)
 
-    #print("width : ", width)
-    #print("height : ", height)
+    matrixNeo = rechercheMatrice.createTableauLed(height, width)
 
-    matrixNeo = rechercheMatrice.createTableauLed(height,width)
+    num_pixels = height*width
+    pixel_pin = board.D18
+    ORDER = neopixel.GRB
+
+    pixels = neopixel.NeoPixel(pixel_pin, num_pixels,brightness=0.2,auto_write=False,pixel_order=ORDER )
 
     # dim = (16, 16)
     dim = (width, height)
@@ -38,18 +45,16 @@ def convert_file(ImageUrl):
             matrixRGB[i, j, 0] = resized[i, j, 0]
             matrixRGB[i, j, 1] = resized[i, j, 1]
             matrixRGB[i, j, 2] = resized[i, j, 2]
-            #print(rechercheMatrice.retourneLedNumero(matrixNeo,i,j))
 
-    #for i in range(height):
-        #for j in range(width):
-            #print(rechercheMatrice.retourneLedNumero(matrixNeo,i,j))
-
-
-    #print(matrixRGB)
+    for i in range(height):
+        for j in range(width):
+            pixels[rechercheMatrice.retourneLedNumero(matrixNeo,i,j)] = (matrixRGB[i, j, 0], matrixRGB[i, j, 1], matrixRGB[i, j, 2])
 
 
 
-    cv2.imshow("resultat en 16x16", matrixRGB)
+    time.sleep(10)
+
+    #cv2.imshow("resultat en 16x16", matrixRGB)
     #cv2.waitKey(0)
     #cv2.destroyAllWindows()
     print('image uploadé !')
