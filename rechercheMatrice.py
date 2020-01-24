357bcc14a76e569725a210c80fc4e6df914b5b8c
JordanDabricot <jordan.dabricot@gmail.com>
2020-01-24 10:38:05 +0000

    Ajout de l'attribution manuelle des numéros de LEDs pour corriger l'affectation suite aux deux LEDs grillées

--- rechercheMatrice.py
+++ rechercheMatrice.py
@@ -1,33 +1,37 @@
-def createTableauLed(width, heigth):
-    #nbPas = 16
+def createTableauLed(width, height):
+	#Déterminer la led en fonction de x et y
+	nbPas = 16
 
-    liste = []
+	liste = []
 
-    for i in range (0, heigth):
-        liste.append([0] * width)
+	for i in range (0, height):
+		liste.append([0] * nbPas)
 
-    cpt = 0
-    for i in range (0, heigth):
-        for j in range (0,width):
-            liste[i][j] = cpt
-            cpt+= 1
+	cpt = 0
+	for i in range (0, height):
+		if cpt == 64:
+			print(cpt)
+			cpt -= 1
+			print(cpt)
+		for j in range (0,width):
+			liste[i][j] = cpt
+			cpt+= 1
 
-    for i in range (1, len(liste)):
-        if i%2 != 0:
-            liste[i].reverse()
-            i+=1
+	for i in range (1, len(liste)):
+		if i%2 != 0:
+			liste[i].reverse()
+		i+=1
 
-    liste.reverse()
-    return liste
+	liste.reverse()
+	return liste
 
 def retourneLedNumero(liste, x,y):
-    return liste[y][x]
+	return liste[y][x]
 
-width = 16
-heigth=16
+monTableauLed = createTableauLed(16,16)
 
-monTableauLed = createTableauLed(width, heigth)
+print(monTableauLed)
 
-#print(monTableauLed)
 
-#print(retourneLedNumero(monTableauLed, 15, 15))
+
+
