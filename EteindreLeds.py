2794e5cbd4e6e5e14459c8d832b4a5644677ab11
Mylene Balech <mylenebalech@MacBook-Pro-de-Mylene.local>
2020-01-24 12:06:54 +0100

    corection ereur

--- EteindreLeds.py
+++ EteindreLeds.py
@@ -1,5 +1,5 @@
-import neopixel
-import board
+#import neopixel
+#import board
 import rechercheMatrice
 def eteindreLeds():
 	num_pixels = 16 * 16
@@ -8,6 +8,8 @@ def eteindreLeds():
 
 	pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
 	mesLeds = rechercheMatrice.createTableauLed(16,16)
-	for l in mesLeds:
-		pixels[l] = (0, 0, 0)
-	pixels.show()
+	for l in (0,len(mesLeds)):
+		for i in range (0,len(mesLeds[l])):
+			print(rechercheMatrice.retourneLedNumero(mesLeds, i,l))
+			pixels[rechercheMatrice.retourneLedNumero(mesLeds, i,l)] = (0, 0, 0)
+		pixels.show()
