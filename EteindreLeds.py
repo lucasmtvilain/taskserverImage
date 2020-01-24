5e4882994609ae1004c7bfcd2c72a6c286caa619
Mylene Balech <mylenebalech@MacBook-Pro-de-Mylene.local>
2020-01-24 12:21:02 +0100

    correction

--- EteindreLeds.py
+++ EteindreLeds.py
@@ -6,11 +6,9 @@ def eteindreLeds():
 	pixel_pin = board.D18
 	ORDER = neopixel.GRB
 
-	pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
+	pixels = neopixel.NeoPixel(pixel_pin, 254, brightness=0.2, auto_write=False, pixel_order=ORDER)
 	mesLeds = rechercheMatrice.createTableauLed(16,16)
 	print("je suis la")
 	for l in (0,254):
 		pixels[l] = (0, 0, 0)
 	pixels.show()
-
-	time.sleep(10)
